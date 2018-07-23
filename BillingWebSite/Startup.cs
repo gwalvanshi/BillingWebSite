3c6910bf18a9fe4bf7e139547cfb3afffe854c84
gwalvanshi <gwalvanshi@gmail.com>
2018-07-23 09:21:28 +0530

    Revert "Revert "Added all files""
    This reverts commit dea7d35ffe12fbe651d202ca77a6cdacdf89dcdc.

---
+++ BillingWebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BillingWebSite.Startup))]
+namespace BillingWebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
